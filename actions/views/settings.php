c099b22668e8ff9697be03a1da336b7a128056ee
Alexander Skakunov <askakunov@itera-research.com>
2018-10-14 12:23:56 +0300

    Login, logout and settings page

--- actions/views/settings.php
+++ actions/views/settings.php
@@ -42,6 +42,24 @@
 <form method="post" enctype="multipart/form-data" onsubmit="$('#loader').show();">
    <table border="0" align="center">
     <tr>
+      <td><label for="user_password">Change the password:</label></td>
+      <td width="10px">&nbsp;</td>
+      <td><input type="password" name="user_password" id="user_password" class="edt" /></td>
+      <td>&nbsp;</td>
+      <td><input id="user_submit" type="Submit" name="user_submit" value="Save" class="btn btn-primary" style="padding: 3px 15px" /></td>
+    </tr>
+    <tr>
+      <td colspan="5">&nbsp;</td>
+    </tr>
+  </table>
+</form>
+
+
+
+<hr/>
+<form method="post" enctype="multipart/form-data" onsubmit="$('#loader').show();">
+   <table border="0" align="center">
+    <tr>
       <td><input type="submit" name="erase_database" id="erase_database" class="btn btn-primary" value="Erase the whole database" /></td>
     </tr>
     <tr>
