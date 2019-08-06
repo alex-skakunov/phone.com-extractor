d022fed666a5c48a3ca1ff16c0ba6b419ffc7617
Alexander Skakunov <alex.skakunov@gmail.com>
2019-08-06 05:33:23 -0700

    Auto-kill hanged imports before an auto-update

--- actions/controllers/grab-file.php
+++ actions/controllers/grab-file.php
@@ -2,6 +2,9 @@
 
 $way = strtolower(trim($_GET['way'])) == 'manual' ? 'manual' : 'auto';
 
+// kill hanged
+query("UPDATE `import_stats` SET `status`='fail', `error_message`='Hanged' WHERE `status`='in progress'  AND DATE_ADD(`started_at`, INTERVAL 1 HOUR) < NOW()");
+
 $isInProgress = query('SELECT `id` FROM `import_stats` WHERE status="in progress" LIMIT 1')->fetchAll();
 if( !empty($isInProgress)) {
     exit('Import in progress');
